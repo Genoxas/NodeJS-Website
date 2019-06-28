de07225563f627824dc3028d348e5c5259e8f644
Genoxa <briantat.cs@gmail.com>
2019-06-27 23:50:10 -0400

    Updates to Briantat.ca website

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+const https = require('https');
+const fs = require('fs');
 var http = require('http');
 var express = require('express');
 var admin = require('firebase-admin');
 var vhost = require('vhost');
 
+
 var tatillia = express();
 
 var briantat = express();
+var SSLPort = 13337;
 var port = 8080;
 var path = require('path');
 
@@ -81,3 +85,20 @@ app.use(express.static('tatillia'));
 app.listen(port, '0.0.0.0', () =>{
     console.log("Server running on port " + port);
 });
+
+const options = {
+    key: fs.readFileSync(__dirname + '/certs/briantat.key', 'utf8'),
+    cert: fs.readFileSync(__dirname + '/certs/briantat.public.cert', 'utf8')
+};
+
+var app2 = module.exports = express();
+app2.use(vhost('*.briantat.ca', briantat));
+app2.use(express.static('briantat'));
+
+var server = https.createServer(options, app2).listen(SSLPort, '0.0.0.0', () =>{
+    console.log("Server running on port " + SSLPort);
+});
+
+//app2.listen(13337, '0.0.0.0', () =>{
+//    console.log("Server running on port " + 13337);
+//});
